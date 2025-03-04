b05a3bb1265ea4da4f879b5f88e27435768671a5
s30660 <s30660@pjwstk.edu.pl>
2025-03-04 13:21:36 +0100

    Liczenie avg

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,16 +1,36 @@
 ﻿public class Program {
+    public static double Average(int[] arr) {
+        double result = 0;
+        if (arr.Length == 0) return result;
+
+        foreach (int i in arr)
+        {
+            result += i;
+        }
+
+        return 1.0 * result / arr.Length;
+    }
+
     public static void Main(string[] args)
     {
-        List<string> list = new List<string>() { 
+        Console.WriteLine(Average(new int[]{
+            1, 2, 3, 4
+        }));
+
+        /*List<string> list = new List<string>() { 
             "a",
             "b",
             "c"
         };
 
+        HashSet<string> set = new HashSet<string>();
+
+        int[] a = { 1, 2, 3 };
+
         foreach (string s in list)
         {
             Console.WriteLine(s);
-        }
+        }*/
         // comment
         // c
         // a
